47a8c488571438cae42b756ca3b0bf2fa4597372
Anastasia Kamysheva <anastasiia.kamysheva@outlook.com>
2025-08-27 11:52:24 +0400

    no more create all on app start

--- analyzer-vacancy/app/db/database.py
+++ analyzer-vacancy/app/db/database.py
@@ -4,17 +4,13 @@ from app.config import settings
 
 
 engine = create_async_engine(settings.database_url, echo=True)
-async_session = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+async_session = async_sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False
+)
 
 Base = declarative_base()
+
 
 async def get_db():
     async with async_session() as session:
         yield session
-
-async def create_models():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-    return {'ok': True}
-
-
--- analyzer-vacancy/app/main.py
+++ analyzer-vacancy/app/main.py
@@ -1,14 +1,12 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.routers import vacancies, all_vacancies_from_db, delete_vacancy
-from app.db.database import create_models
 from app.tasks import scheduler, start_scheduler
 from app.routers import parser
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    await create_models()
     start_scheduler()
     yield
     scheduler.shutdown()
