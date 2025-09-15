8a6dc5d014ec616c25438611b2973566343df290
Anastasia Kamysheva <anastasiia.kamysheva@outlook.com>
2025-09-15 11:51:03 +0400

    removed unnecessary code

--- analyzer-vacancy/app/services/service_hh.py
+++ analyzer-vacancy/app/services/service_hh.py
@@ -2,15 +2,6 @@ import asyncio
 from httpx import AsyncClient
 
 
-# async def get_vacancies(query: str = 'Python', per_page: int = 100):
-#     url = 'https://api.hh.ru/vacancies'
-#     params = {'text': query, 'per_page': per_page}
-#     async with AsyncClient() as client:
-#         response = await client.get(url, params=params)
-#         response.raise_for_status()
-#         return response.json()['items']
-
-
 class HHCollectorVacancies:
     def __init__(self, query: str = "Python", per_page: int = 100):
         self.query = query
