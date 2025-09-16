c15dc9c3eb3869a9bbc6b149a9f6bd9a7004b474
Anastasia Kamysheva <anastasiia.kamysheva@outlook.com>
2025-09-16 11:52:39 +0400

    corrected the names

--- analyzer-vacancy/app/config.py
+++ analyzer-vacancy/app/config.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).parent.parent.parent
 
 
 class Settings(BaseSettings):
-    USER_DB: str
-    PASSWORD_DB: str
-    NAME_DB: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
     HOST_DB: str
     PORT_DB: int
     GEMINI_API_KEY: str
@@ -16,7 +16,7 @@ class Settings(BaseSettings):
 
     @property
     def database_url(self):
-        return f"postgresql+asyncpg://{self.USER_DB}:{self.PASSWORD_DB}@{self.HOST_DB}:{self.PORT_DB}/{self.NAME_DB}"
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.HOST_DB}:{self.PORT_DB}/{self.POSTGRES_DB}"
 
     model_config = SettingsConfigDict(env_file="app/.env")
 
